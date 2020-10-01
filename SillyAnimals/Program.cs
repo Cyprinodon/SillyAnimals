982ede838e6547d9e8d473fed8a7f858984c089b
grabette dimitri <cyprinodon@hotmail.fr>
2020-10-01 19:34:45 +0200

    Ajoutez des fichiers projet.

---
+++ SillyAnimals/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SillyAnimals.Main
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Salut, royaume des animaux !");
+            Console.ReadKey();
+        }
+    }
+}
